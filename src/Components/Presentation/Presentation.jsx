fc9f71adf5ba0c6eedf52d13ed415cf4222ef3f8
Juane003 <j.emilio164@gmail.com>
2022-08-29 13:13:14 -0300

    changed presentation pptx and link

--- src/Components/Presentation/Presentation.jsx
+++ src/Components/Presentation/Presentation.jsx
@@ -1,5 +1,5 @@
 import { Carousel } from "react-responsive-carousel";
-import { Diapositiva1, Diapositiva2, Diapositiva3, Diapositiva4, Diapositiva5, Diapositiva6, Diapositiva7, Diapositiva8, Diapositiva9, Diapositiva10 } from "../../assets/slides";
+import { Diapositiva1, Diapositiva2, Diapositiva3, Diapositiva4, Diapositiva5, Diapositiva6, Diapositiva7, Diapositiva8, Diapositiva9, Diapositiva10, Diapositiva11, Diapositiva12, Diapositiva13, Diapositiva14, Diapositiva15, Diapositiva16, Diapositiva17 } from "../../assets/slides";
 import "./presentation.css";
 
 const Presentation = () => {
@@ -11,23 +11,30 @@ const Presentation = () => {
                 autoPlay 
                 transitionTime="500" 
                 infiniteLoop 
-                //showArrows={false}
+                showArrows={false}
                 showIndicators={false}
-                //showStatus={false}
                 showThumbs={false}
             >
                 <img src={Diapositiva1} className="slide-img"/>
                 <img src={Diapositiva2} className="slide-img"/>
                 <img src={Diapositiva3} className="slide-img"/>
                 <img src={Diapositiva4} className="slide-img"/>
+                <img src={Diapositiva17} className="slide-img"/>
                 <img src={Diapositiva5} className="slide-img"/>
                 <img src={Diapositiva6} className="slide-img"/>
                 <img src={Diapositiva7} className="slide-img"/>
                 <img src={Diapositiva8} className="slide-img"/>
                 <img src={Diapositiva9} className="slide-img"/>
                 <img src={Diapositiva10} className="slide-img"/>
+                <img src={Diapositiva11} className="slide-img"/>
+                <img src={Diapositiva12} className="slide-img"/>
+                <img src={Diapositiva13} className="slide-img"/>
+                <img src={Diapositiva14} className="slide-img"/>
+                <img src={Diapositiva15} className="slide-img"/>
+                <img src={Diapositiva16} className="slide-img"/>
+
             </Carousel>
-            <a href="https://frsnutneduar-my.sharepoint.com/:p:/g/personal//jrivero_frsn_utn_edu_ar/EaxxwgvfRctCp5MfiMcODvEBFcTo4SXF_-RqoPqy09ncDA?e=Isz3vK" target="_blank" className="download-btn">Abrir Presentacion</a>
+            <a href="https://frsnutneduar-my.sharepoint.com/:p:/r/personal/jrivero_frsn_utn_edu_ar/_layouts/15/Doc.aspx?sourcedoc=%7BFA21FF19-02F4-4215-99B8-99BEE1EEA15A%7D&file=INTRODUCCION%20A%20LOS%20S.O.%20(5)%20-%20Copia.pptx&action=edit&mobileredirect=true&DefaultItemOpen=1&login_hint=jrivero%40frsn.utn.edu.ar&ct=1661789522292&wdOrigin=OFFICECOM-WEB.START.EDGEWORTH&cid=e780aee6-343e-48d9-b117-351f1c9f92d4" target="_blank" className="download-btn">Abrir Presentacion</a>
         </div>
     )
 }
--- src/assets/slides/index.js
+++ src/assets/slides/index.js
@@ -8,6 +8,13 @@ import Diapositiva7 from '@/assets/slides/Diapositiva7.PNG';
 import Diapositiva8 from '@/assets/slides/Diapositiva8.PNG';
 import Diapositiva9 from '@/assets/slides/Diapositiva9.PNG';
 import Diapositiva10 from '@/assets/slides/Diapositiva10.PNG';
+import Diapositiva11 from '@/assets/slides/Diapositiva11.PNG';
+import Diapositiva12 from '@/assets/slides/Diapositiva12.PNG';
+import Diapositiva13 from '@/assets/slides/Diapositiva13.PNG';
+import Diapositiva14 from '@/assets/slides/Diapositiva14.PNG';
+import Diapositiva15 from '@/assets/slides/Diapositiva15.PNG';
+import Diapositiva16 from '@/assets/slides/Diapositiva16.PNG';
+import Diapositiva17 from '@/assets/slides/Diapositiva17.PNG';
 
 export {
   Diapositiva1,
@@ -19,5 +26,12 @@ export {
   Diapositiva7,
   Diapositiva8,
   Diapositiva9,
-  Diapositiva10
+  Diapositiva10,
+  Diapositiva11,
+  Diapositiva12,
+  Diapositiva13,
+  Diapositiva14,
+  Diapositiva15,
+  Diapositiva16,
+  Diapositiva17
 }
